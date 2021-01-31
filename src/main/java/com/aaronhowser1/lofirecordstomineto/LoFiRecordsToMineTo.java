6798197c11f5194c132766e3ef39ba5bbffaa3b3
TheSilkMiner <thesilkminer@outlook.com>
2021-01-31 20:43:52 +0100

    Rework registration according to latest Forge guidelines
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/com/aaronhowser1/lofirecordstomineto/LoFiRecordsToMineTo.java
+++ src/main/java/com/aaronhowser1/lofirecordstomineto/LoFiRecordsToMineTo.java
@@ -1,131 +1,24 @@
 package com.aaronhowser1.lofirecordstomineto;
 
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Rarity;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("lofirecordstomineto")
+@Mod(LoFiRecordsToMineTo.MOD_ID)
 public class LoFiRecordsToMineTo {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "lofirecordstomineto";
 
     public LoFiRecordsToMineTo() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+        ModItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ModSoundEvents.SOUND_EVENTS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-    }
-
-    public static ItemGroup LoFiGroup = new ItemGroup("lofirecordstomineto") {
+    public static final ItemGroup LO_FI_GROUP = new ItemGroup(MOD_ID) {
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(ModItems.INTERDIMENSIONAL);
+            return new ItemStack(ModItems.INTERDIMENSIONAL.get());
         }
     };
-
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-
-        private static Item createItemModRecord(ResourceLocation resourceLocation, SoundEvent soundEvent) {
-            return new ItemModRecord(0, soundEvent, (new Item.Properties().maxStackSize(1).group(LoFiGroup).rarity(Rarity.RARE))).setRegistryName(resourceLocation);
-        }
-
-        @SubscribeEvent
-        public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
-            event.getRegistry().registerAll(
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "a_little_something"), ModSoundEvents.MUSIC_DISC_A_LITTLE_SOMETHING),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "a_mix_of_time_and_space"), ModSoundEvents.MUSIC_DISC_A_MIX_OF_SOUND_AND_SPACE),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "alone"), ModSoundEvents.MUSIC_DISC_ALONE),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "face_it"), ModSoundEvents.MUSIC_DISC_FACE_IT),
-//                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "faces"), ModSoundEvents.FACES),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "falling_leaves"), ModSoundEvents.MUSIC_DISC_FALLING_LEAVES),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "finger_puppets"), ModSoundEvents.MUSIC_DISC_FINGER_PUPPETS),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "falls_first_sunday"), ModSoundEvents.MUSIC_DISC_FALLS_FIRST_SUNDAY),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "happy_birthday"), ModSoundEvents.MUSIC_DISC_HAPPY_BIRTHDAY),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "in_search_of_life"), ModSoundEvents.MUSIC_DISC_IN_SEARCH_OF_LIFE),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "interdimensional"), ModSoundEvents.MUSIC_DISC_INTERDIMENSIONAL),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "its_inside"), ModSoundEvents.MUSIC_DISC_ITS_INSIDE),
-//                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "keyhole"), ModSoundEvents.KEYHOLE),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "look_at_the_camera"), ModSoundEvents.MUSIC_DISC_LOOK_AT_THE_CAMERA),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "making_ways"), ModSoundEvents.MUSIC_DISC_MAKING_WAYS),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "memory_loss"), ModSoundEvents.MUSIC_DISC_MEMORY_LOSS),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "normal"), ModSoundEvents.MUSIC_DISC_NORMAL),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "overhead"), ModSoundEvents.MUSIC_DISC_OVERHEAD),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "plucked"), ModSoundEvents.MUSIC_DISC_PLUCKED),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "remember"), ModSoundEvents.MUSIC_DISC_REMEMBER),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "render"), ModSoundEvents.MUSIC_DISC_RENDER),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "running_from_giants"), ModSoundEvents.MUSIC_DISC_RUNNING_FROM_GIANTS),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "shadow_people"), ModSoundEvents.MUSIC_DISC_SHADOW_PEOPLE),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "space_whales"), ModSoundEvents.MUSIC_DISC_SPACE_WHALES),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "survived_by"), ModSoundEvents.MUSIC_DISC_SURVIVED_BY),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "there_there"), ModSoundEvents.MUSIC_DISC_THERE_THERE),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "thiccer"), ModSoundEvents.MUSIC_DISC_THICKER),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "time_rider"), ModSoundEvents.MUSIC_DISC_TIME_RIDER),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "turning_over_rocks"), ModSoundEvents.MUSIC_DISC_TURNING_OVER_ROCKS),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "uncanny"), ModSoundEvents.MUSIC_DISC_UNCANNY),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "war"), ModSoundEvents.MUSIC_DISC_WAR),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "when_it_rains"), ModSoundEvents.MUSIC_DISC_WHEN_IT_RAINS),
-                    createItemModRecord(new ResourceLocation("lofirecordstomineto", "wind"), ModSoundEvents.MUSIC_DISC_WIND)
-            );
-        }
-
-        @SubscribeEvent
-        public static void onSoundRegistry(final RegistryEvent.Register<SoundEvent> event) {
-            event.getRegistry().registerAll(ModSoundEvents.MUSIC_DISC_A_LITTLE_SOMETHING,
-                    ModSoundEvents.MUSIC_DISC_ALONE,
-                    ModSoundEvents.MUSIC_DISC_A_MIX_OF_SOUND_AND_SPACE,
-                    ModSoundEvents.MUSIC_DISC_FACE_IT,
-                    ModSoundEvents.MUSIC_DISC_FALLING_LEAVES,
-                    ModSoundEvents.MUSIC_DISC_A_LITTLE_SOMETHING,
-                    ModSoundEvents.MUSIC_DISC_A_MIX_OF_SOUND_AND_SPACE,
-                    ModSoundEvents.MUSIC_DISC_ALONE,
-                    ModSoundEvents.MUSIC_DISC_FACE_IT,
-//                    ModSoundEvents.MUSIC_DISC_FACES,
-                    ModSoundEvents.MUSIC_DISC_FALLING_LEAVES,
-                    ModSoundEvents.MUSIC_DISC_FINGER_PUPPETS,
-                    ModSoundEvents.MUSIC_DISC_FALLS_FIRST_SUNDAY,
-                    ModSoundEvents.MUSIC_DISC_HAPPY_BIRTHDAY,
-                    ModSoundEvents.MUSIC_DISC_IN_SEARCH_OF_LIFE,
-                    ModSoundEvents.MUSIC_DISC_INTERDIMENSIONAL,
-                    ModSoundEvents.MUSIC_DISC_ITS_INSIDE,
-//                    ModSoundEvents.MUSIC_DISC_KEYHOLE,
-                    ModSoundEvents.MUSIC_DISC_LOOK_AT_THE_CAMERA,
-                    ModSoundEvents.MUSIC_DISC_MAKING_WAYS,
-                    ModSoundEvents.MUSIC_DISC_MEMORY_LOSS,
-                    ModSoundEvents.MUSIC_DISC_NORMAL,
-                    ModSoundEvents.MUSIC_DISC_OVERHEAD,
-                    ModSoundEvents.MUSIC_DISC_PLUCKED,
-                    ModSoundEvents.MUSIC_DISC_REMEMBER,
-                    ModSoundEvents.MUSIC_DISC_RENDER,
-                    ModSoundEvents.MUSIC_DISC_RUNNING_FROM_GIANTS,
-                    ModSoundEvents.MUSIC_DISC_SHADOW_PEOPLE,
-                    ModSoundEvents.MUSIC_DISC_SPACE_WHALES,
-                    ModSoundEvents.MUSIC_DISC_SURVIVED_BY,
-                    ModSoundEvents.MUSIC_DISC_THERE_THERE,
-                    ModSoundEvents.MUSIC_DISC_THICKER,
-                    ModSoundEvents.MUSIC_DISC_TIME_RIDER,
-                    ModSoundEvents.MUSIC_DISC_TURNING_OVER_ROCKS,
-                    ModSoundEvents.MUSIC_DISC_UNCANNY,
-                    ModSoundEvents.MUSIC_DISC_WAR,
-                    ModSoundEvents.MUSIC_DISC_WHEN_IT_RAINS,
-                    ModSoundEvents.MUSIC_DISC_WIND);
-        }
-    }
 }
--- src/main/java/com/aaronhowser1/lofirecordstomineto/ModSoundEvents.java
+++ src/main/java/com/aaronhowser1/lofirecordstomineto/ModSoundEvents.java
@@ -2,43 +2,46 @@ package com.aaronhowser1.lofirecordstomineto;
 
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
-@SuppressWarnings("all")
 public class ModSoundEvents {
+    public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, LoFiRecordsToMineTo.MOD_ID);
 
-    public static final SoundEvent MUSIC_DISC_A_LITTLE_SOMETHING        = create(new ResourceLocation("lofirecordstomineto", "a_little_something"));
-    public static final SoundEvent MUSIC_DISC_A_MIX_OF_SOUND_AND_SPACE  = create(new ResourceLocation("lofirecordstomineto", "a_mix_of_sound_and_space"));
-    public static final SoundEvent MUSIC_DISC_ALONE                     = create(new ResourceLocation("lofirecordstomineto", "alone"));
-    public static final SoundEvent MUSIC_DISC_FACE_IT                   = create(new ResourceLocation("lofirecordstomineto", "face_it"));
-    public static final SoundEvent MUSIC_DISC_FALLING_LEAVES            = create(new ResourceLocation("lofirecordstomineto", "falling_leaves"));
-    public static final SoundEvent MUSIC_DISC_FINGER_PUPPETS            = create(new ResourceLocation("lofirecordstomineto", "finger_puppets"));
-    public static final SoundEvent MUSIC_DISC_FALLS_FIRST_SUNDAY        = create(new ResourceLocation("lofirecordstomineto", "falls_first_sunday"));
-    public static final SoundEvent MUSIC_DISC_HAPPY_BIRTHDAY            = create(new ResourceLocation("lofirecordstomineto", "happy_birthday"));
-    public static final SoundEvent MUSIC_DISC_IN_SEARCH_OF_LIFE         = create(new ResourceLocation("lofirecordstomineto", "in_search_of_life"));
-    public static final SoundEvent MUSIC_DISC_INTERDIMENSIONAL          = create(new ResourceLocation("lofirecordstomineto", "interdimensional"));
-    public static final SoundEvent MUSIC_DISC_ITS_INSIDE                = create(new ResourceLocation("lofirecordstomineto", "its_inside"));
-    public static final SoundEvent MUSIC_DISC_LOOK_AT_THE_CAMERA        = create(new ResourceLocation("lofirecordstomineto", "look_at_the_camera"));
-    public static final SoundEvent MUSIC_DISC_MAKING_WAYS               = create(new ResourceLocation("lofirecordstomineto", "making_ways"));
-    public static final SoundEvent MUSIC_DISC_MEMORY_LOSS               = create(new ResourceLocation("lofirecordstomineto", "memory_loss"));
-    public static final SoundEvent MUSIC_DISC_NORMAL                    = create(new ResourceLocation("lofirecordstomineto", "normal"));
-    public static final SoundEvent MUSIC_DISC_OVERHEAD                  = create(new ResourceLocation("lofirecordstomineto", "overhead"));
-    public static final SoundEvent MUSIC_DISC_PLUCKED                   = create(new ResourceLocation("lofirecordstomineto", "plucked"));
-    public static final SoundEvent MUSIC_DISC_REMEMBER                  = create(new ResourceLocation("lofirecordstomineto", "remember"));
-    public static final SoundEvent MUSIC_DISC_RENDER                    = create(new ResourceLocation("lofirecordstomineto", "render"));
-    public static final SoundEvent MUSIC_DISC_RUNNING_FROM_GIANTS       = create(new ResourceLocation("lofirecordstomineto", "running_from_giants"));
-    public static final SoundEvent MUSIC_DISC_SHADOW_PEOPLE             = create(new ResourceLocation("lofirecordstomineto", "shadow_people"));
-    public static final SoundEvent MUSIC_DISC_SPACE_WHALES              = create(new ResourceLocation("lofirecordstomineto", "space_whales"));
-    public static final SoundEvent MUSIC_DISC_SURVIVED_BY               = create(new ResourceLocation("lofirecordstomineto", "survived_by"));
-    public static final SoundEvent MUSIC_DISC_THERE_THERE               = create(new ResourceLocation("lofirecordstomineto", "there_there"));
-    public static final SoundEvent MUSIC_DISC_THICKER                   = create(new ResourceLocation("lofirecordstomineto", "thicker"));
-    public static final SoundEvent MUSIC_DISC_TIME_RIDER                = create(new ResourceLocation("lofirecordstomineto", "time_rider"));
-    public static final SoundEvent MUSIC_DISC_TURNING_OVER_ROCKS        = create(new ResourceLocation("lofirecordstomineto", "turning_over_rocks"));
-    public static final SoundEvent MUSIC_DISC_UNCANNY                   = create(new ResourceLocation("lofirecordstomineto", "uncanny"));
-    public static final SoundEvent MUSIC_DISC_WAR                       = create(new ResourceLocation("lofirecordstomineto", "war"));
-    public static final SoundEvent MUSIC_DISC_WHEN_IT_RAINS             = create(new ResourceLocation("lofirecordstomineto", "when_it_rains"));
-    public static final SoundEvent MUSIC_DISC_WIND                      = create(new ResourceLocation("lofirecordstomineto", "wind"));
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_A_LITTLE_SOMETHING = create("a_little_something");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_A_MIX_OF_SOUND_AND_SPACE = create("a_mix_of_sound_and_space");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_ALONE = create("alone");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_FACE_IT = create("face_it");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_FALLING_LEAVES = create("falling_leaves");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_FINGER_PUPPETS = create("finger_puppets");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_FALLS_FIRST_SUNDAY = create("falls_first_sunday");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_HAPPY_BIRTHDAY = create("happy_birthday");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_IN_SEARCH_OF_LIFE = create("in_search_of_life");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_INTERDIMENSIONAL = create("interdimensional");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_ITS_INSIDE = create("its_inside");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_LOOK_AT_THE_CAMERA = create("look_at_the_camera");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_MAKING_WAYS = create("making_ways");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_MEMORY_LOSS = create("memory_loss");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_NORMAL = create("normal");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_OVERHEAD = create("overhead");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_PLUCKED = create("plucked");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_REMEMBER = create("remember");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_RENDER = create("render");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_RUNNING_FROM_GIANTS = create("running_from_giants");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_SHADOW_PEOPLE = create("shadow_people");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_SPACE_WHALES = create("space_whales");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_SURVIVED_BY = create("survived_by");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_THERE_THERE = create("there_there");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_THICKER = create("thicker");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_TIME_RIDER = create("time_rider");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_TURNING_OVER_ROCKS = create("turning_over_rocks");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_UNCANNY = create("uncanny");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_WAR = create("war");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_WHEN_IT_RAINS = create("when_it_rains");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_WIND = create("wind");
 
-    private static SoundEvent create(ResourceLocation name) {
-        return new SoundEvent(name).setRegistryName(name);
+    private static RegistryObject<SoundEvent> create(String name) {
+        return SOUND_EVENTS.register(name, () -> new SoundEvent(new ResourceLocation(LoFiRecordsToMineTo.MOD_ID, name)));
     }
 }
