ab334f2eeab12eeacd7f6b61f963b0554c0a09bc
Aaron Howser <aaronhowser1@gmail.com>
2022-11-06 05:36:02 -0600

    items in but nonfunctional

---
+++ src/main/java/com/aaronhowser1/lofirecordstomineto/LoFiRecordsToMineTo.java
@@ -0,0 +1,32 @@
+package com.aaronhowser1.lofirecordstomineto;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(LoFiRecordsToMineTo.MOD_ID)
+public class LoFiRecordsToMineTo
+{
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "lofirecordstomineto";
+
+    public LoFiRecordsToMineTo() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+}
