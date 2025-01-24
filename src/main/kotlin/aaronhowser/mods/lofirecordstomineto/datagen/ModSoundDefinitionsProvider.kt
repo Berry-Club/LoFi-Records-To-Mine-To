67a108f3c146c35168b460b75137f37f3751061e
Aaron Howser <aaronhowser1@gmail.com>
2025-01-24 13:32:23 -0600

    improved function

--- src/main/kotlin/aaronhowser/mods/lofirecordstomineto/datagen/ModSoundDefinitionsProvider.kt
+++ src/main/kotlin/aaronhowser/mods/lofirecordstomineto/datagen/ModSoundDefinitionsProvider.kt
@@ -3,24 +3,32 @@ package aaronhowser.mods.lofirecordstomineto.datagen
 import aaronhowser.mods.lofirecordstomineto.LoFiRecordsToMineTo
 import aaronhowser.mods.lofirecordstomineto.registry.ModSounds
 import net.minecraft.data.PackOutput
+import net.minecraft.sounds.SoundEvent
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import net.neoforged.neoforge.common.data.SoundDefinition
 import net.neoforged.neoforge.common.data.SoundDefinitionsProvider
+import net.neoforged.neoforge.registries.DeferredHolder
 
 class ModSoundDefinitionsProvider(
     output: PackOutput,
     helper: ExistingFileHelper
 ) : SoundDefinitionsProvider(output, LoFiRecordsToMineTo.ID, helper) {
 
-    private fun s(name: String) = SoundDefinition.definition()
-        .with(sound(LoFiRecordsToMineTo.getRl(name)))
+    private fun singleSound(soundEvent: DeferredHolder<SoundEvent, SoundEvent>) {
+        val name = soundEvent.key!!.location().path
+
+        this.add(
+            soundEvent,
+            SoundDefinition.definition()
+                .with(
+                    sound(LoFiRecordsToMineTo.getRl(name))
+                )
+        )
+    }
 
     override fun registerSounds() {
 
-        add(
-            ModSounds.A_MIX_OF_SOUND_AND_SPACE,
-            s("a_mix_of_sound_and_space")
-        )
+        singleSound(ModSounds.A_MIX_OF_SOUND_AND_SPACE)
 
     }
 }
