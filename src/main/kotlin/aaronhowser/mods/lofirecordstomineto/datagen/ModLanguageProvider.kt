06598c45a8996d6ee66de4de24a10fd34041b76c
Aaron Howser <aaronhowser1@gmail.com>
2025-01-24 12:59:42 -0600

    working on datagen

---
+++ src/main/kotlin/aaronhowser/mods/lofirecordstomineto/datagen/ModLanguageProvider.kt
@@ -0,0 +1,26 @@
+package aaronhowser.mods.lofirecordstomineto.datagen
+
+import aaronhowser.mods.lofirecordstomineto.LoFiRecordsToMineTo
+import aaronhowser.mods.lofirecordstomineto.ModItems
+import net.minecraft.data.PackOutput
+import net.neoforged.neoforge.common.data.LanguageProvider
+
+class ModLanguageProvider(
+    output: PackOutput
+) : LanguageProvider(output, LoFiRecordsToMineTo.ID, "en_us") {
+
+    companion object {
+
+    }
+
+    override fun addTranslations() {
+        addItems()
+    }
+
+    private fun addItems() {
+
+        addItem(ModItems.A_LITTLE_SOMETHING, "A Little Something")
+
+    }
+
+}
